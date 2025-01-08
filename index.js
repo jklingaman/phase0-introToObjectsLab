dbcf3ccf87d005113561620b025049cdd1628550
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-01-08 12:05:55 -0600

    passes every test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = { employeeName: "Sam" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const employees = {...employee};
+    employees[key] = value;
+    return employees;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const employees = {...employee};
+    delete employees[key];
+    return employees;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
